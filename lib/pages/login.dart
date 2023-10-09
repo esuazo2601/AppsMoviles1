0356cc4337ed8ff5944a51badc9ddbb7a9b2321d
esuazo2601 <esteban.suazo.m2601@gmail.com>
2023-10-09 18:32:36 -0300

    Pantalla de login lista señores

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -16,74 +16,85 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text("Login"),
-        backgroundColor: Colors.grey,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          Center(child: Image.asset('assets/escudo.gif')),
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 70,
-            ),
-            Container(
-              color: Colors
-                  .white, // Color de fondo para la sección de usuario y contraseña
-              padding: const EdgeInsets.all(
-                  20.0), // Espaciado interno del contenedor
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Text("Usuario"),
-                  TextFormField(
-                    decoration:
-                        const InputDecoration(border: OutlineInputBorder()),
-                    controller: _userController,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Text("Contraseña"),
-                  TextFormField(
-                    decoration:
-                        const InputDecoration(border: OutlineInputBorder()),
-                    controller: _passwordController,
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  if (_userController.text == "dido" &&
-                      _passwordController.text == "123") {
-                    print("Loguear");
-                  } else {
-                    MotionToast.error(
-                            title: const Text("Error"),
-                            description:
-                                const Text("Usuario o contraseña incorrectos"))
-                        .show(context);
-                  }
-                },
-                child: const Text("ACCEDER")),
-          ],
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text("Acceder"),
+          backgroundColor: Colors.grey,
         ),
-      ),
-      floatingActionButton: ElevatedButton(
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const Register()));
-        },
-        child: const Text("Registrarse"),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Center(
+                    child: Image.asset(
+                  'assets/escudo.gif',
+                  height: 150,
+                  width: 250,
+                )),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  color: Colors
+                      .white, // Color de fondo para la sección de usuario y contraseña
+                  padding: const EdgeInsets.all(
+                      20.0), // Espaciado interno del contenedor
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text("Usuario"),
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(border: OutlineInputBorder()),
+                        controller: _userController,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text("Contraseña"),
+                      TextFormField(
+                        obscureText: true,
+                        decoration:
+                            const InputDecoration(border: OutlineInputBorder()),
+                        controller: _passwordController,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_userController.text == "dido" &&
+                          _passwordController.text == "123") {
+                        print("Loguear");
+                      } else {
+                        MotionToast.error(
+                                title: const Text("Error"),
+                                description: const Text(
+                                    "Usuario o contraseña incorrectos"))
+                            .show(context);
+                      }
+                    },
+                    child: const Text("ACCEDER")),
+                const SizedBox(
+                  height: 180,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Register()));
+                  },
+                  child: const Text("Registrarse"),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
